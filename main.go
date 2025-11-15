1c40d4fb346e55f0e42090f4f7df2faa3c52f45a
backendStark <239644803+backendStark@users.noreply.github.com>
2025-11-15 19:17:28 +0500

    Rewrite 10 request to goroutines

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -10,21 +11,31 @@ const googleURL = "https://google.com/"
 
 func main() {
 	t := time.Now()
+	var wg sync.WaitGroup
+
 	for i := range 10 {
-		res, err := http.Get(googleURL)
-
-		if err != nil {
-			fmt.Println("error get response from google")
-			return
-		}
-
-		defer res.Body.Close()
-
-		if res.StatusCode >= 200 && res.StatusCode < 300 {
-			fmt.Printf("%d. Response from google successful, status code: %d\n", i+1, res.StatusCode)
-		} else {
-			fmt.Printf("%d. error get response from google, status code: %d\n", i+1, res.StatusCode)
-		}
+		wg.Add(1)
+		go doReqToGoogle(i, &wg)
 	}
+
+	wg.Wait()
 	fmt.Println(time.Since(t))
+}
+
+func doReqToGoogle(index int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	res, err := http.Get(googleURL)
+
+	if err != nil {
+		fmt.Println("error get response from google")
+		return
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode >= 200 && res.StatusCode < 300 {
+		fmt.Printf("%d. Response from google successful, status code: %d\n", index+1, res.StatusCode)
+	} else {
+		fmt.Printf("%d. error get response from google, status code: %d\n", index+1, res.StatusCode)
+	}
 }
