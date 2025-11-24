d6fdfb0e27e8967d664fa60bcf871a7c7cb02454
backendStark <239644803+backendStark@users.noreply.github.com>
2025-11-24 18:32:16 +0500

    start simple http server

--- main.go
+++ main.go
@@ -1,45 +1,19 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net/http"
-	"os"
-	"strings"
 )
 
+const serverPortNumber = "8081"
+const serverPort = ":" + serverPortNumber
+
 func main() {
-	path := flag.String("file", "urls.txt", "path to URL file")
-	flag.Parse()
-
-	file, err := os.ReadFile(*path)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	urlSlice := strings.Split(string(file), "\n")
-	respCh := make(chan int)
-	errCh := make(chan error)
-
-	for _, url := range urlSlice {
-		go ping(url, respCh, errCh)
-	}
-
-	for i := 0; i < len(urlSlice); i++ {
-		res := <- respCh
-		fmt.Println(res)
-		errRes := <- errCh
-		fmt.Println(errRes)
-	}
+	fmt.Println("Server start and listening port:", serverPortNumber)
+	http.HandleFunc("/hello", helloHandler)
+	http.ListenAndServe(serverPort, nil)
 }
 
-func ping(url string, respCh chan int, errCh chan error) {
-	resp, err := http.Get(url)
-
-	if err != nil {
-		errCh <- err
-		return
-	}
-
-	respCh <- resp.StatusCode
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("hello")
 }
