f3dacb7f800eba06d376e373f202f9326a930141
backendStark <239644803+backendStark@users.noreply.github.com>
2025-12-13 22:41:35 +0500

    implement update link method

--- internal/link/handler.go
+++ internal/link/handler.go
@@ -5,6 +5,9 @@ import (
 	"net/http"
 	"shorten/pkg/req"
 	"shorten/pkg/res"
+	"strconv"
+
+	"gorm.io/gorm"
 )
 
 type LinkHandlerDeps struct {
@@ -64,7 +67,32 @@ func (handler *LinkHandler) Create() http.HandlerFunc {
 }
 func (handler *LinkHandler) Update() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		body, err := req.HandleBody[LinkUpdateRequest](&w, r)
+		if err != nil {
+			return
+		}
 
+		idString := r.PathValue("id")
+		id, err := strconv.ParseUint(idString, 10, 32)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		link, err := handler.LinkRepository.Update(&Link{
+			Model: gorm.Model{
+				ID: uint(id),
+			},
+			Url:  body.Url,
+			Hash: body.Hash,
+		})
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		res.JSON(w, 201, link)
 	}
 }
 func (handler *LinkHandler) Delete() http.HandlerFunc {
--- internal/link/repository.go
+++ internal/link/repository.go
@@ -1,6 +1,10 @@
 package link
 
-import "shorten/pkg/db"
+import (
+	"shorten/pkg/db"
+
+	"gorm.io/gorm/clause"
+)
 
 type LinkRepository struct {
 	Database *db.Db
@@ -26,4 +30,12 @@ func (repo *LinkRepository) GetByHash(hash string) (*Link, error) {
 		return nil, result.Error
 	}
 	return &link, nil
+}
+
+func (repo *LinkRepository) Update(link *Link) (*Link, error) {
+	result := repo.Database.DB.Clauses(clause.Returning{}).Updates(link)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return link, nil
 }
