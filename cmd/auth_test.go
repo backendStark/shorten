af097027a9fbdf627a053068f21a5465156a9ccb
backendStark <239644803+backendStark@users.noreply.github.com>
2025-12-18 22:17:39 +0500

    add tests for login

---
+++ cmd/auth_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"shorten/internal/auth"
+	"testing"
+)
+
+func TestLoginSuccess(t *testing.T) {
+	ts := httptest.NewServer(App())
+	defer ts.Close()
+
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email:    "a@a.com",
+		Password: "1",
+	})
+
+	res, err := http.Post(ts.URL+"/auth/login", "application/json", bytes.NewReader(data))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode != 200 {
+		t.Fatalf("Expected status code 200, got %d", res.StatusCode)
+	}
+
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var resData auth.LoginResponse
+	err = json.Unmarshal(body, &resData)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resData.Token == "" {
+		t.Fatal("Expected token, got empty")
+	}
+}
+
+func TestLoginFail(t *testing.T) {
+	ts := httptest.NewServer(App())
+	defer ts.Close()
+
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email:    "a@a.com",
+		Password: "1",
+	})
+
+	res, err := http.Post(ts.URL+"/auth/login", "application/json", bytes.NewReader(data))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if res.StatusCode != 401 {
+		t.Fatalf("Expected status code 401, got %d", res.StatusCode)
+	}
+}
